4d8df2a0d6a438020cce3e3ec62788c150f1baa2
Sebastian Schäfer <seba.sjannu@gmail.com>
2022-01-30 14:00:16 +0100

    Added necessary parameters for the EGSSimulation class

--- topasgraphsim/src/classes/sim_import.py
+++ topasgraphsim/src/classes/sim_import.py
@@ -85,3 +85,16 @@ class EGSSimulation:
 
         self.filepath = filepath
         self.filename = self.filepath.split("/")[-1][:-7]
+
+        self.direction = ""
+        self.unit = ""
+        self.dose = []
+        self.std_dev = []
+        self.axis = []
+
+        self.axis = {True: self.axis[len(self.axis) // 2 :], False: self.axis}
+        self.std_dev = {
+            True: self.std_dev[len(self.std_dev) // 2 :],
+            False: self.std_dev,
+        }
+        self.normpoint = max(self.dose)
