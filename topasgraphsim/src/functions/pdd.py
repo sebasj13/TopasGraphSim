Fixed pdd parameter calculation for ptwTba Scans

topasgraphsim/src/functions/pdd.py
@@ -11,10 +11,6 @@ def calculate_parameters(axis, dose, std_dev):
     descriptive parameters of depth doses.
     """
 
-    axis = np.flip(axis).tolist()
-    dose = np.flip(dose)
-    std_dev = np.flip(std_dev)
-
     if type(std_dev) != np.ndarray or std_dev == []:
         TD20 = dose[(np.abs(axis - 200)).argmin()]
         TD10 = dose[(np.abs(axis - 100)).argmin()]
@@ -22,6 +18,10 @@ def calculate_parameters(axis, dose, std_dev):
         zmax = round(axis[int(np.where(dose == max(dose))[0][0])], 5)
 
         return [Q, 0, zmax]
+
+    axis = np.flip(axis).tolist()
+    dose = np.flip(dose)
+    std_dev = np.flip(std_dev)
 
     new_step = max(
         [