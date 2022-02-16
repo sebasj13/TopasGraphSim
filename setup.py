058f1365568a83cd2a54049c7d4a6dd8339ad9d6
Sebastian Schäfer <seba.sjannu@gmail.com>
2022-02-16 13:29:57 +0100

    Update to Version 18.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="topasgraphsim",
-    version="18.0.2",
+    version="18.1.0",
     author="Sebastian Schäfer",
     author_email="sebastian.schaefer@student.uni-halle.de",
     description="GUI to plot the results of a topas simulation",
