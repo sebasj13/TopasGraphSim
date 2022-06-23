Bug fix preventing simulations without a standard deviation from loading

topasgraphsim/src/classes/sim_import.py
@@ -56,7 +56,7 @@ class Simulation:
                 else:
                     self.std_dev = self.std_dev / np.sqrt(self.histories)
             else:
-                std_dev = np.array([])
+                self.std_dev = np.array([])
 
         if self.direction != "Z":
             self.axis = np.array(