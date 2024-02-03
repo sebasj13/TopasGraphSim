Fixed plateu normalization for off-axis data

topasgraphsim/src/classes/tgs_graph.py
@@ -63,14 +63,22 @@ class TGS_Plot():
                 dose = np.divide(dose,np.max(dose))
 
             elif self.normalization == "plateau":
-                l = len(dose)//2
-                error = np.divide(error,np.average(dose[l-5:l+5]))
-                dose = np.divide(dose,np.average(dose[l-5:l+5]))
+
+                max_dose_index = np.argmax(dose)
+                left_index = max_dose_index
+                right_index = max_dose_index
+                while left_index > 0 and dose[left_index] >= 0.9 * dose[max_dose_index]:
+                    left_index -= 1
+                while right_index < len(dose) - 1 and dose[right_index] >= 0.9 * dose[max_dose_index]:
+                    right_index += 1
+                plateau_width =  right_index - left_index
+                average_dose = np.average(dose[max_dose_index - plateau_width // 2 : max_dose_index + plateau_width // 2])
+                error = np.divide(error, average_dose)
+                dose = np.divide(dose, average_dose)
 
             elif self.normalization == "centeraxis":
                 error = np.divide(error,dose[len(dose)//2])
                 dose = np.divide(dose,dose[len(dose)//2])
-
 
         dose = np.add(dose * self.dosefactor, self.doseshift)
         error = np.add(error * self.dosefactor, self.doseshift)